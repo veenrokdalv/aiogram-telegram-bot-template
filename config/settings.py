d06044251efa79499e6e8fc2689819b9cd1e60fd
veenrokroot <veenrokdalv@gmail.com>
2022-11-14 22:23:53 +0300

    Добавлен poetry.lock

--- config/settings.py
+++ config/settings.py
@@ -8,12 +8,10 @@ ENV_FILE = BASE_DIR / '.env'
 LOGGING_CONF_FILE = BASE_DIR / 'logging.conf.json'
 
 env = Env()
+
 env.read_env(str(ENV_FILE))
 
 TELEGRAM_BOT_TOKENS = env.list('TELEGRAM_BOT_TOKENS')
 PARSE_MODE = 'HTML'
-
-
-
 
 del env
