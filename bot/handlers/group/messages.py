4ae5005ee299bcf31ca1881bc90373a6e3dc9e65
= <veenrokdalv@gmail.com>
2022-11-23 07:32:00 +0300

    Добавлен пример определения хендлера

--- bot/handlers/group/messages.py
+++ bot/handlers/group/messages.py
@@ -2,6 +2,15 @@ from aiogram import Dispatcher, Router
 
 router = Router()
 
+# from aiogram.types import Message
+# from bot.filters import ChatTypesFilter
+# from bot.utils.throttling import rate_limit
+
+# @rate_limit()
+# @router.message(ChatTypesFilter('group', 'supergroup'))
+# async def handler_echo(message: Message):
+#     await message.answer(text=message.text)
+
 
 def setup(*, dispatcher: Dispatcher):
     dispatcher.include_router(router)
