69c161df1b992c338dcc455110988bcf1508dcdc
veenrokdalv <veenrokdalv@protonmail.com>
2022-12-09 10:51:52 +0300

    Удален setup_webhook

--- bot/utils/bot.py
+++ bot/utils/bot.py
@@ -1,22 +1,7 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot
 from aiogram.utils.i18n import I18n
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp.web import run_app
-from aiohttp.web_app import Application
 
 import loggers
-
-
-def setup_webhook(*bots: Bot, dispatcher: Dispatcher, **extra_data):
-    loggers.bot.debug('Setup webhooks')
-    webhook_url_postfix = 'webhook/{bot.id}'
-    web_app = Application()
-    for bot in bots:
-        web_handler = SimpleRequestHandler(dispatcher=dispatcher, bot=bot, **extra_data)
-        web_handler.register(web_app, path='/' + webhook_url_postfix.format(bot=bot))
-
-    setup_application(web_app, dispatcher, bots=bots, **extra_data)
-    run_app(app=web_app, host='127.0.0.1', port=8080)
 
 
 async def set_my_commands(bot: Bot, i18n: I18n):
