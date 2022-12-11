afa06bd806e4207ba16ee8e5155caa9d0de6245b
veenrokdalv <veenrokdalv@protonmail.com>
2022-12-11 12:14:36 +0300

    Cleanup

--- bot/handlers/hooks.py
+++ bot/handlers/hooks.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Router, Dispatcher
 from aiogram.utils.i18n import I18n
 
 from bot.utils.bot import set_my_commands
+from config import settings
 
 router = Router()
 
@@ -10,22 +11,20 @@ router = Router()
 async def on_startup(bots: list[Bot], i18n: I18n, **kwargs):
     for bot in bots:
         await set_my_commands(bot=bot, i18n=i18n)
-        if 'webhook_url' in kwargs:
-            await bot.set_webhook(
-                url=kwargs['webhook_url'].format(bot_token=bot.token),
-                secret_token=kwargs['secret_key'],
-                drop_pending_updates=kwargs['drop_pending_updates'],
-            )
+        await bot.set_webhook(
+            url=settings.WEBHOOK_URL.format(bot_token=bot.token),
+            secret_token=settings.SECRET_KEY,
+            drop_pending_updates=True,
+        )
 
 
 @router.shutdown()
 async def on_shutdown(bots: list[Bot], i18n: I18n, **kwargs):
     for bot in bots:
         await bot.delete_my_commands()
-        if 'webhook_url' in kwargs:
-            await bot.delete_webhook(
-                drop_pending_updates=kwargs['drop_pending_updates'],
-            )
+        await bot.delete_webhook(
+            drop_pending_updates=True,
+        )
 
 
 def setup(*, dispatcher: Dispatcher):
